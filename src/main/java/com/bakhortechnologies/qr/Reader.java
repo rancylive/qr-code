4d11eae3d462c978ae1e02549dd9b81e1495de14
rancylive <choudhury.ranjan22@gmail.com>
2022-05-08 10:02:00 +0530

    Create Reader.java

---
+++ src/main/java/com/bakhortechnologies/qr/Reader.java
@@ -0,0 +1,39 @@
+package com.bakhortechnologies.qr;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Reader {
+    private static String decodeQRCode(File qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("./qrCode.png");
+            String decodedText = decodeQRCode(file);
+            if(decodedText == null) {
+                System.out.println("No QR Code found in the image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to read QR Code " + e.getMessage());
+        }
+    }
+}
